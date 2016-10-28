e8f1250342831f99d4e66fa1eac5f3eed0a5983a
Ishank Tandon <itandon95@gmail.com>
2016-10-27 23:03:53 -0400

    added auth for users

--- backend/BeerUpServer/routes/people.js
+++ backend/BeerUpServer/routes/people.js
@@ -44,4 +44,60 @@ router.route('/')
     });
   });
 
+// router.param('username', function (req, res, next, username) {
+//   // console.log(username);
+//     mongoose.model('People').find({"username": username}, function (err, user) {
+//         if (err || user === null) {
+//             res.status(404);
+//             err = new Error('Not Found');
+//             err.status = 404;
+//             res.format({
+//                 json: function () {
+//                     res.json({ message: err.status + ' ' + err });
+//                 }
+//             });
+//         } else {
+//             req.username = username;
+//             console.log("blah" + req.username);
+//             next();
+//             console.log('gets here');
+//         }
+//     });
+// });
+
+router.route('/:username/:password')
+  .get(function (req, res) {
+    console.log(req.params.username);
+      mongoose.model('People').findOne({ username: req.params.username, hashedPassword: req.params.password }, function (err, user) {
+          if (err) {
+              res.status(404);
+              err = new Error('GET error, problem retrieving data');
+              err.status = 404;
+              res.format({
+                  json: function () {
+                      res.json({ message: err.status + ' ' + err });
+                  }
+              });
+          } else {
+            if (user === null) {
+                res.status(401);
+                err = new Error('Unauthorized user. Enter correct credentials');
+                err.status = 401;
+                res.format({
+                    json: function () {
+                        res.json({ message: err.status + ' ' + err });
+                    }
+                });
+            } else {
+              // res.format({
+              //     json: function () {
+                      res.send("Authorized.. Yayyy!!!");
+                  // }
+              // });
+            }
+          }
+      });
+  });
+
+
   module.exports = router;
