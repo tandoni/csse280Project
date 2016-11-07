b41d093b4a9f66659bdffdb269ee1d51e48eeeb6
Chi Zhang <superzcdalian@gmail.com>
2016-11-07 17:47:53 -0500

    front end first commit

---
+++ public/js/login.js
@@ -0,0 +1,16 @@
+$('#login').on('click', function() {
+    console.log('clicked');
+    $.ajax({
+        url: 'http://127.0.0.1:3000/people/login/' + $('#username').val() + '/' + $('#password').val(),
+        type: 'POST',
+        success: function(data) {
+            var expire = new Date().getTime() + 86400
+            var forLocal = {token: data.token, expire: expire};
+            localStorage.setItem("webToken", JSON.stringify(forLocal));
+            console.log(data);
+        },
+        error: function(request, status, error) {
+            alert(error);
+        }
+    });
+});
