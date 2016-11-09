f07a051216c8873f8e3457f2dc1023b70df87328
Ishank Tandon <itandon95@gmail.com>
2016-11-09 15:56:54 -0500

    changed path

--- frontend/public/js/review.js
+++ frontend/public/js/review.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
        var token = JSON.parse(localStorage.getItem('webToken'));
         if (token.expire < Date.now()) {
             $.ajax({
-                url: 'http://localhost:3000/reviews/addreview/' + $('#beerName').val() + '/' + $('#breweryName').val() + '/' + $('#rating').val() + '/' + $('#review').val(),
+                url: 'https://csse280-beerup-backend.herokuapp.com/reviews/addreview/' + $('#beerName').val() + '/' + $('#breweryName').val() + '/' + $('#rating').val() + '/' + $('#review').val(),
                 type: 'POST',
                 data: token,
                 success: function(data) {
