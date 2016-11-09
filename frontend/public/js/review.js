736ed332212d6ee5dd4f370286e5585d0c27c906
Ishank Tandon <itandon95@gmail.com>
2016-11-09 16:03:17 -0500

    fixed error

--- frontend/public/js/review.js
+++ frontend/public/js/review.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#addReview').on('click', function() {
        console.log('asdasd');
        var token = JSON.parse(localStorage.getItem('webToken'));
-        if (token.expire < Date.now()) {
+        if (token.expire > Date.now()) {
             $.ajax({
                 url: 'https://csse280-beerup-backend.herokuapp.com/reviews/addreview/' + $('#beerName').val() + '/' + $('#breweryName').val() + '/' + $('#rating').val() + '/' + $('#review').val(),
                 type: 'POST',
