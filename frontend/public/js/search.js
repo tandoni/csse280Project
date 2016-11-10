3c54db07d3d632638ae02dfe95de4e6bcaa6df97
Chi Zhang <superzcdalian@gmail.com>
2016-11-09 19:20:29 -0500

    add beer in search done

--- frontend/public/js/search.js
+++ frontend/public/js/search.js
@@ -4,41 +4,40 @@ var ALL_QUOTES = [{
         index: "0",
         image: "../images/1.jpg",
         introduction: "Welcome every morning with a smile. Look on the new day as another special gift from your Creator, another golden opportunity.",
-        beer: "Og Mandino",
+        beer: "Og Mandino"
     },
     {
         index: "1",
         image: "../images/2.gif",
         introduction: "Happiness cannot be traveled to, owned, earned, or worn.It is the spiritual experience of living every minute with love, grace & gratitude.",
-        beer: "Denis Waitley",
+        beer: "Denis Waitley"
     },
     {
         index: "2",
         image: "../images/3.jpeg",
         introduction: "Though no one can go back and make a brand new start, anyone can start from now and make a brand new ending",
-        beer: "Carl Bard",
+        beer: "Carl Bard"
     },
     {
         index: "3",
         image: "../images/4.jpg",
         introduction: "Accept responsibility for your life.Know that it is you who will get you where you want to go, no one else.",
-        beer: "Les Brown",
+        beer: "Les Brown"
     },
     {
         index: "4",
         image: "../images/5.jpg",
         introduction: "Surround yourself with only people who are going to lift you higher.",
-        beer: "German",
+        beer: "German"
     },
     {
         index: "5",
         image: "../images/craft-beer.jpg",
         introduction: "Nobody ever wrote down a plan to be broke, fat, lazy, or stupid.Those things are what happen when you don’t have a plan.",
-        beer: "Larry Winget",
+        beer: "Larry Winget"
     }
 ];
 
-//popup modal
 // Get the modal
 var modal = document.getElementById('myModal');
 
@@ -51,19 +50,39 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
-}
 
+    //When user click submit, add to db
+    var addbeer = document.getElementById("addbeer");
+    addbeer.addEventListener("click", function() {
+        addQuote();
+        displayAllQuotes();
+
+    });
+};
+
+function addQuote() {
+    var beername = document.getElementById("beer").value;
+    var description = document.getElementById("description").value;
+    var imgURL = document.getElementById("imgURL").value;
+
+
+    //All data here, ready
+    ALL_QUOTES.push({ index: ALL_QUOTES.length, image: imgURL, introduction: description, beer: beername });
+    alert(ALL_QUOTES.length);
+
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-}
+};
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// };
+
 
 // Use this to store user's choice
 var userChoice = 'all';
@@ -95,10 +114,11 @@ function makeQuotesWhoseBeersNameStartWith(prefix) {
     var quotes = [];
     return function() {
         ALL_QUOTES.forEach(function(object, index) {
-            if (object.beer[0] == prefix || prefix == "") {
+            if (object.beer[0] == prefix || prefix === "") {
                 quotes.push(object);
             }
-        })
+        });
+
         return quotes;
     };
 }
